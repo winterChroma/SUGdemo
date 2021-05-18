4518daf007ae0cf125931a0033d3fc01cfcc8bff
Levi Ebora <levi.ebora@gmail.com>
2021-05-18 16:41:39 +0800

    env var for beanstalk

--- db_conn.php
+++ db_conn.php
@@ -1,13 +1,13 @@
 <?php
 
-// $dbhost = $_SERVER['RDS_HOSTNAME'];
-// $dbport = $_SERVER['RDS_PORT'];
-// $dbname = $_SERVER['RDS_DB_NAME'];
-// $charset = 'utf8' ;
+$dbhost = $_SERVER['RDS_HOSTNAME'];
+$dbport = $_SERVER['RDS_PORT'];
+$dbname = $_SERVER['RDS_DB_NAME'];
+$charset = 'utf8' ;
 
-// $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
-// $username = $_SERVER['RDS_USERNAME'];
-// $password = $_SERVER['RDS_PASSWORD'];
+$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
+$username = $_SERVER['RDS_USERNAME'];
+$password = $_SERVER['RDS_PASSWORD'];
 
 try {
 
